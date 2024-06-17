a8b238ef520916b1b227bcd413d7283946c32b7a
wllmrodr <141373533+wllmrodr@users.noreply.github.com>
2024-06-17 13:16:01 -0300

    mudei só alguns detalhes por enquanto

--- controllers/GuiApplication.java
+++ controllers/GuiApplication.java
@@ -7,6 +7,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class GuiApplication extends Application {
+
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         // Carrega o arquivo FXML
@@ -15,14 +21,18 @@ public class GuiApplication extends Application {
         // Configura o título da janela principal
         primaryStage.setTitle("Super Trunfo");
 
+        Image icon = new Image("views/assets/icon.png");
+        primaryStage.getIcons().add(icon);
+
         // Define a cena com o root carregado do FXML e um tamanho inicial razoável
         primaryStage.setScene(new Scene(root, 800, 600)); // Ajusta tamanho conforme necessário
+
+        // Deixa em tela cheia
+        primaryStage.setFullScreen(true);
 
         // Exibe a janela
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
 }
