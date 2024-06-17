b945c79f56b107576fe1deaf1a2381ad06c530ea
grigogi <103059907+grigogi@users.noreply.github.com>
2024-06-17 14:53:49 -0300

    BaralhoGeral.java

--- models/BaralhoGeral.java
+++ models/BaralhoGeral.java
@@ -1,17 +1,33 @@
 package models;
-
 import java.util.ArrayList;
-import java.util.Collections;  // Importa a classe Collections para embaralhamento
+import java.util.Collections;
 
 public class BaralhoGeral extends Baralho {
     public BaralhoGeral() {
         super();
         this.baralho = new ArrayList<Carta>();
     }
-    
+
     // Método para embaralhar as cartas no baralho
     public void embaralhar() {
-        Collections.shuffle(baralho);  // Embaralha o ArrayList de Carta
+        Collections.shuffle(baralho);
+    }
+
+    // Método para distribuir cartas entre dois jogadores
+    public void distribuirCartas(Jogador jogador, JogadorMaquina jogadorMaquina) {
+        int totalCartas = baralho.size();
+        int metade = totalCartas / 2;
+
+        // Distribui a primeira metade das cartas para o jogador
+        for (int i = 0; i < metade; i++) {
+            Carta carta = baralho.remove(0);
+            jogador.receberCarta(carta);
+        }
+
+        // Distribui a segunda metade das cartas para o jogador máquina
+        while (!baralho.isEmpty()) {
+            Carta carta = baralho.remove(0);
+            jogadorMaquina.receberCarta(carta);
+        }
     }
 }
-
