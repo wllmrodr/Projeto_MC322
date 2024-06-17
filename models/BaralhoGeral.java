60c9aa3c38acde008f092f92b4396eec2f1fdbc3
grigogi <103059907+grigogi@users.noreply.github.com>
2024-06-17 14:42:44 -0300

    BaralhoGeral.java

--- models/BaralhoGeral.java
+++ models/BaralhoGeral.java
@@ -1,10 +1,17 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.Collections;  // Importa a classe Collections para embaralhamento
 
-public class BaralhoGeral extends Baralho{
-    public BaralhoGeral(){
+public class BaralhoGeral extends Baralho {
+    public BaralhoGeral() {
         super();
         this.baralho = new ArrayList<Carta>();
     }
+    
+    // Método para embaralhar as cartas no baralho
+    public void embaralhar() {
+        Collections.shuffle(baralho);  // Embaralha o ArrayList de Carta
+    }
 }
+
