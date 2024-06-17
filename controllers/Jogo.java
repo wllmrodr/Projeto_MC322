d8e5b3b82310dd7d6fc50a7922d250d5eaf3e147
grigogi <103059907+grigogi@users.noreply.github.com>
2024-06-17 20:33:41 -0300

    Jogo.java

--- controllers/Jogo.java
+++ controllers/Jogo.java
@@ -1,5 +1,4 @@
 package controllers;
-
 import models.*;
 
 import java.util.Random;
@@ -8,71 +7,113 @@ import java.util.Scanner;
 public class Jogo {
     private JogadorMaquina jogadorMaquina;
     private JogadorReal jogadorReal;
-    private  BaralhoGeral baralhoGeral;
+    private BaralhoGeral baralhoGeral;
 
     public Jogo(JogadorMaquina jogadorMaquina, JogadorReal jogadorReal, BaralhoGeral baralhoGeral) {
         this.jogadorMaquina = jogadorMaquina;
         this.jogadorReal = jogadorReal;
         this.baralhoGeral = baralhoGeral;
     }
+
     public BaralhoGeral getBaralhoGeral() {
         return baralhoGeral;
     }
 
-    private boolean verificaSeAcabou(){
-        if (jogadorReal.getBaralhoMao().getBaralho().size() == 32){
-            System.out.println("PARABÉNS! VOCE GANHOU!!");
+    private boolean verificaSeAcabou() {
+        if (jogadorReal.getBaralhoMao().getBaralho().size() == 32) {
+            System.out.println("PARABÉNS! VOCÊ GANHOU!!");
             return false;
         }
-        if (jogadorMaquina.getBaralhoMao().getBaralho().size() == 32){
+        if (jogadorMaquina.getBaralhoMao().getBaralho().size() == 32) {
             System.out.println("Infelizmente o computador te superou no trunfo...");
             return false;
         }
         return true;
     }
 
-    public void loopDeJogo(){
+    private boolean compararCategoria(String categoria) {
+        boolean empate = false;
+        while (true) {
+            Carta cartaJogador = jogadorReal.getBaralhoMao().getBaralho().get(0);
+            Carta cartaMaquina = jogadorMaquina.getBaralhoMao().getBaralho().get(0);
 
-        baralhoGeral.embaralhar();
-        baralhoGeral.distribuirCartas(jogadorReal, jogadorMaquina);
-        Random rand = new Random();
-        int aleatorio = rand.nextInt(2);
-        boolean vencedor;
-        if(aleatorio == 0){
-            vencedor = true;
-        }else {
-            vencedor = false;
-        }
-        //se vencedor = true, rodada jogador Real. Se false, computador que escolhe.
+            float valorJogador = 0, valorMaquina = 0;
+            switch (categoria) {
+                case "1":
+                    valorJogador = cartaJogador.getFofura();
+                    valorMaquina = cartaMaquina.getFofura();
+                    break;
+                case "2":
+                    valorJogador = cartaJogador.getAgilidade();
+                    valorMaquina = cartaMaquina.getAgilidade();
+                    break;
+                case "3":
+                    valorJogador = cartaJogador.getAgressividade();
+                    valorMaquina = cartaMaquina.getAgressividade();
+                    break;
+                case "4":
+                    valorJogador = cartaJogador.getBrincalhao();
+                    valorMaquina = cartaMaquina.getBrincalhao();
+                    break;
+                case "5":
+                    valorJogador = cartaJogador.getObediencia();
+                    valorMaquina = cartaMaquina.getObediencia();
+                    break;
+                default:
+                    System.out.println("Escolha inválida. Tente novamente.");
+                    return false;
+            }
 
-        boolean parada = true;
-        while (parada){
-
-            if(vencedor){
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Qual atributo você escolhe? Digite o número do atributo desejado.\n\n" +
-                        "1) Fofura\n2) Agilidade\n3) Agressividade\n4) Brincalhão\n5)Obediência");
-                String escolha = scanner.nextLine();
-                switch (escolha){
-                    case "1":
-
-                }
-                parada = verificaSeAcabou();
-
+            if (valorJogador > valorMaquina) {
+                jogadorReal.ganharCartaDeOutroJogador(jogadorMaquina, cartaMaquina);
+                return true;
+            } else if (valorJogador < valorMaquina) {
+                jogadorMaquina.ganharCartaDeOutroJogador(jogadorReal, cartaJogador);
+                return false;
+            } else {
+                empate = true;
+                jogadorReal.getBaralhoMao().colocarCartaNoFim(cartaJogador);
+                jogadorMaquina.getBaralhoMao().colocarCartaNoFim(cartaMaquina);
             }
         }
     }
 
-    public void verCartasJogo(){
-        for (Carta carta : baralhoGeral.getBaralho()){
-            System.out.println("Nome: "+ carta.getNome()
-                    + "\nFofura: "+ carta.getFofura()
-                    + "\nAgilidade: "+ carta.getAgilidade()
-                    + "\nAgressividade: "+ carta.getAgressividade()
-                    + "\nBrincalhão: "+ carta.getBrincalhao()
-                    + "\nObediência: "+ carta.getObediencia()
-                    + "\n"
-            );
+    public void loopDeJogo() {
+        baralhoGeral.embaralhar();
+        baralhoGeral.distribuirCartas(jogadorReal, jogadorMaquina);
+        Random rand = new Random();
+        int aleatorio = rand.nextInt(2);
+        boolean vencedor = aleatorio == 0;  // Se vencedor = true, rodada jogador Real. Se false, computador escolhe.
+
+        boolean parada = true;
+        while (parada) {
+            if (vencedor) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Qual atributo você escolhe? Digite o número do atributo desejado.\n\n" +
+                        "1) Fofura\n2) Agilidade\n3) Agressividade\n4) Brincalhão\n5) Obediência");
+                String escolha = scanner.nextLine();
+                vencedor = compararCategoria(escolha);  // Determina o vencedor da próxima rodada com base na comparação
+                parada = verificaSeAcabou();
+            } else {
+                // Lógica para o computador escolher um atributo (simplesmente escolhemos um atributo aleatório aqui)
+                String[] atributos = {"1", "2", "3", "4", "5"};
+                String escolhaComputador = atributos[rand.nextInt(atributos.length)];
+                System.out.println("Computador escolheu o atributo " + escolhaComputador);
+                vencedor = !compararCategoria(escolhaComputador);  // O computador escolhe, então inverte o resultado
+                parada = verificaSeAcabou();
+            }
+        }
+    }
+
+    public void verCartasJogo() {
+        for (Carta carta : baralhoGeral.getBaralho()) {
+            System.out.println("Nome: " + carta.getNome()
+                    + "\nFofura: " + carta.getFofura()
+                    + "\nAgilidade: " + carta.getAgilidade()
+                    + "\nAgressividade: " + carta.getAgressividade()
+                    + "\nBrincalhão: " + carta.getBrincalhao()
+                    + "\nObediência: " + carta.getObediencia()
+                    + "\n");
         }
     }
 }
