992fd5f2cd6c84601b535b35fa1b8a5f7a8b8282
grigogi <103059907+grigogi@users.noreply.github.com>
2024-06-19 20:39:22 -0300

    BaralhoMao.java

--- models/BaralhoMao.java
+++ models/BaralhoMao.java
@@ -3,41 +3,20 @@ package models;
 import java.util.ArrayList;
 
 public class BaralhoMao extends Baralho {
-    private final int limite = 32;
+    private final int limite = 4;
 
     public BaralhoMao() {
         super();
-        this.baralho = new ArrayList<Carta>();
+        this.baralho = new ArrayList<>();
     }
 
-    public int tamanhoBaralho(){
-        return baralho.size();
-    }
-    
-    // Método para colocar uma carta no fim do baralho
     public void colocarCartaNoFim(Carta carta) {
-        if (baralho.size() < limite) {
-            baralho.add(carta);
-        } else {
-            System.out.println("O limite de cartas na mão foi atingido.");
-        }
+        baralho.add(carta);
     }
 
-    // Método para perder uma carta e ela ir para o fim do baralho do outro jogador
-    public void perderCartaParaOutroJogador(BaralhoMao outroJogador, Carta carta) {
-        if (baralho.remove(carta)) {
-            outroJogador.colocarCartaNoFim(carta);
-        } else {
-            System.out.println("A carta não está presente na mão.");
-        }
-    }
-
-    // Método para ganhar uma carta de outro jogador e colocá-la no fim do baralho
-    public void ganharCartaDeOutroJogador(BaralhoMao outroJogador, Carta carta) {
-        if (outroJogador.getBaralho().remove(carta)) {
-            this.colocarCartaNoFim(carta);
-        } else {
-            System.out.println("O outro jogador não possui a carta.");
+    public void perderCarta() {
+        if (!baralho.isEmpty()) {
+            baralho.remove(0);
         }
     }
 }
